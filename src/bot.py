890a4a8402d2892ec79dfff2b573c2499218e204
Jonas Lima <jonaslucasduartelima@outlook.com.br>
2022-12-24 16:18:40 -0300

    Criado sistema de captura de logs

--- src/bot.py
+++ src/bot.py
@@ -1,16 +1,22 @@
 from extends import calcular, verificar_expressao
 from my_token import TOKEN
+from time import ctime, time
 import telebot
+
+log = open(f'log{int(time())}.txt', 'w')
 
 bot = telebot.TeleBot(TOKEN)
 
 bot.calculadora_ativada = False
 
 def qualquer_mensagem(mensagem):
+    print(f"{mensagem.chat.first_name} {mensagem.chat.last_name} : ({ctime(mensagem.date)}) : {mensagem.text}", file=log)
     return True
 
 @bot.message_handler(commands=['ativar'])
 def ativar_calculadora(mensagem):
+
+    print(f"{mensagem.chat.first_name} {mensagem.chat.last_name} : ({ctime(mensagem.date)}) : {mensagem.text}", file=log)
 
     if bot.calculadora_ativada:
         bot.reply_to(mensagem, 'Calculadora já ativada')
@@ -21,13 +27,15 @@ def ativar_calculadora(mensagem):
 @bot.message_handler(commands=['desativar'])
 def desativar_calculadora(mensagem):
 
+    print(f"{mensagem.chat.first_name} {mensagem.chat.last_name} : ({ctime(mensagem.date)}) : {mensagem.text}", file=log)
+
     if not bot.calculadora_ativada:
         bot.reply_to(mensagem, 'Calculadora já desativada')
     else:
         bot.calculadora_ativada = False
         bot.reply_to(mensagem, 'Desativando calculadora...')
 
-@bot.message_handler(func=lambda mensagem : qualquer_mensagem(mensagem.text) and not bot.calculadora_ativada)
+@bot.message_handler(func=lambda mensagem : qualquer_mensagem(mensagem) and not bot.calculadora_ativada)
 def instruir_usuario(mensagem):
 
     texto = """
@@ -51,3 +59,5 @@ def responder_erro(mensagem):
     bot.reply_to(mensagem, 'ERRO! Verifique a expressão!')
 
 bot.polling()
+
+log.close()
